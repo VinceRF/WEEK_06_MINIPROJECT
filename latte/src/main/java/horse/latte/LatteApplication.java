f89250617914af55470eb411787d3e955d7e1b9e
VinceRF <zhsk30@naver.com>
2022-06-10 11:07:47 +0900

    라떼 환경설정 생성

---
+++ latte/src/main/java/horse/latte/LatteApplication.java
@@ -0,0 +1,13 @@
+package horse.latte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatteApplication.class, args);
+    }
+
+}
---
+++ latte/src/test/java/horse/latte/LatteApplicationTests.java
@@ -0,0 +1,13 @@
+package horse.latte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
