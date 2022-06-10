a7586d1b9ce070fbb6629fb8732903c0a51b6801
Mo-Greene <13blueboy13@daum.net>
2022-06-10 18:10:52 +0900

    키워드  - 게시물 entity 작성, 단위  #1
    변경사항 : 테이블작성

---
+++ latte/src/main/java/horse/latte/model/Board.java
@@ -0,0 +1,29 @@
+package horse.latte.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Board extends Timestamped{
+//게시물 엔티티
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                //테이블 id
+
+    @Column(nullable = false)
+    private String nickname;        //작성 닉네임
+
+    @Column(nullable = false)
+    private String content;         //작성 내용
+
+    @Column(nullable = false)
+    private Long year;              //연도별 구분
+
+    @Column(nullable = false)
+    private String url;             //사진 url
+}
