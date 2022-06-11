d4061f4666678cead36fc1530e982af1b800f11b
Mo-Greene <13blueboy13@daum.net>
2022-06-11 22:16:27 +0900

    최신순 전체 게시물조회.  구현 최신순 연도별 게시물 조회.  구현

--- latte/src/main/java/horse/latte/repository/BoardRepository.java
+++ latte/src/main/java/horse/latte/repository/BoardRepository.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    //일단 year 값으로 된 모든걸 가져오는거라고 생각하자
-    List<Board> findByYear(Long year);
+    //Board 전체 조회 최신순으로 가져오기
+    List<Board> findAllByOrderByModifiedAtDesc();
+
+    //일단 year 값으로 된 모든걸 가져오는거라고 생각하자 : 최신순으로
+    List<Board> findByYearByModifiedAtDesc(Long year);
 }
