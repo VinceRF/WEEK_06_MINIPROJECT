d17ca44addb25a3b7066222d685c0af0df292f14
lirongzzuin <younggyun12@hotmail.com>
2022-06-11 21:55:07 +0900

    이거 잘못 올려지면 울거임

---
+++ src/main/java/horse/latte/controller/BoardController.java
@@ -0,0 +1,60 @@
+package horse.latte.controller;
+
+
+import horse.latte.dto.BoardRequestDto;
+import horse.latte.model.Board;
+import horse.latte.model.User;
+import horse.latte.repository.BoardRepository;
+import horse.latte.security.UserDetailsImpl;
+import horse.latte.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+
+    @PostMapping("/api/board")
+    public Board createBoard(@RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 로그인 되어 있는 회원 테이블의 ID
+        return boardService.createBoard(requestDto, userDetails);
+    }
+
+    @GetMapping("/api/board")
+    public List<Board> readBoard(@RequestParam(required = false) Long year) {
+        if (year != null) {
+            return boardService.getBoardsByYear(year);
+        } else {
+            return boardService.getAllBoards();
+        }
+    }
+
+    @PutMapping("/api/board/{id}")
+    public Long updateBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody BoardRequestDto requestDto) {
+        return boardService.update(id, userDetails.getUsername(), requestDto);
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/api/board/{id}")
+    public Long deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return boardService.delete(id, userDetails.getUsername());
+    }
+
+    @GetMapping("/api/board/{id}")
+    public Board getBoard(@PathVariable Long id) {
+        return boardService.getBoard(id);
+    }
+
+//        // year로 조회
+//    @GetMapping("/api/board/years/{year}")
+//    public List<Board> selectedBoard(@PathVariable Long year){
+//        return boardRepository.findAllByYear(year);
+//    }
+}
+
