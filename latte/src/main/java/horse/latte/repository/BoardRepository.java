83936a4518920f57a7cfe6874f7c103a4b1119b2
Mo-Greene <13blueboy13@daum.net>
2022-06-13 11:56:50 +0900

    게시물 Repository 좋아요 기능 추가

--- latte/src/main/java/horse/latte/repository/BoardRepository.java
+++ latte/src/main/java/horse/latte/repository/BoardRepository.java
@@ -2,6 +2,9 @@ package horse.latte.repository;
 
 import horse.latte.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,5 +14,16 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAllByOrderByModifiedAtDesc();
 
     //일단 year 값으로 된 모든걸 가져오는거라고 생각하자 : 최신순으로
-    List<Board> findByYearByModifiedAtDesc(Long year);
+    List<Board> findAllByYear(Long year);
+
+    //게시물 좋아요 구현
+    @Transactional
+    @Modifying
+    @Query("update Board m set m.loveCount = m.loveCount+1 where m.id = :id")
+    int upLoveCount(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("update Board m set m.loveCount = m.loveCount-1 where m.id = :id")
+    int downLoveCount(Long id);
 }
