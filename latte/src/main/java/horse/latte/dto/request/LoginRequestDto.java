7f1dd76783e7e64e89c6ad04b261c5899183b4ea
Mo-Greene <13blueboy13@daum.net>
2022-06-16 02:24:59 +0900

    좋아요 true , false 로 구현

--- latte/src/main/java/horse/latte/dto/request/LoginRequestDto.java
+++ latte/src/main/java/horse/latte/dto/request/LoginRequestDto.java
@@ -1,14 +1,8 @@
 package horse.latte.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginRequestDto {
     private Long id;
     private String password;
