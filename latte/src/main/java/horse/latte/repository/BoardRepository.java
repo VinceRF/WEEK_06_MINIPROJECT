46e386bf715f2ae22c80a2a6c11d59205164a4d9
lirongzzuin <younggyun12@hotmail.com>
2022-06-16 21:32:56 +0900

    final commit

--- latte/src/main/java/horse/latte/repository/BoardRepository.java
+++ latte/src/main/java/horse/latte/repository/BoardRepository.java
@@ -14,5 +14,4 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAllByYear(Long year);
 //    List<Board> findAllByUserId(Long userId);
     Optional<Board> findById(Long id);
-
 }
