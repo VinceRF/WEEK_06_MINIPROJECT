795aa39ca05f540e8a3e9fc3fa90c1d1aac77c45
VinceRF <zhsk30@naver.com>
2022-06-16 17:31:59 +0900

    @Param 추가

--- latte/src/main/java/horse/latte/repository/BoardRepository.java
+++ latte/src/main/java/horse/latte/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import horse.latte.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,13 +20,13 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Transactional
     @Modifying
     @Query("update Board m set m.loveCount = m.loveCount+1 where m.id = :id")
-    int uplikeCount(Long id);
+    int uplikeCount(@Param("id") Long id);
     //해당 아이디의 카운터를 1 업,
 
     @Transactional
     @Modifying
     @Query("update Board m set m.loveCount = m.loveCount-1 where m.id = :id")
-    int downlikeCount(Long id);
+    int downlikeCount(@Param("id") Long id);
     //해당 아이디의 카운터를 1 다운.,
 
 
